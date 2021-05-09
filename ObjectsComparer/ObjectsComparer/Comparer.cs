2d9d4db4cb233d496e03262cd788fd4ac0f0e4fb
Ladislav Sopko <ladislav.sopko@gmail.com>
2021-05-09 23:15:21 +0200

    WIP -> net5.0

--- ObjectsComparer/ObjectsComparer/Comparer.cs
+++ ObjectsComparer/ObjectsComparer/Comparer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using ObjectsComparer.Utils;
+using MemberInfoExtensions = ObjectsComparer.Utils.MemberInfoExtensions;
 
 namespace ObjectsComparer
 {
